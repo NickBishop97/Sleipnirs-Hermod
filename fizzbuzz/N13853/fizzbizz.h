857e3a6220f69da88b23c52d7873d8d737fd2fdf
n13853 <Maxwell.Rosales@ngc.com>
2022-06-07 17:57:03 -0400

    bug fix 1

---
+++ fizzbuzz/N13853/fizzbizz.h
@@ -0,0 +1,12 @@
+#ifndef FIZZBIZZ_H
+#define FIZZBIZZ_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+void fizzbuzz(
+    unsigned int lowerLimit, 
+    unsigned int upperLimit, 
+    std::ostream& os);
+#endif 
