fdb631632bd981f85ec4ac1ade8f21cf3aaa824b
n08066 <Matthew.Hendrickson2@ngc.com>
2022-06-10 19:07:54 -0400

    Fixing pretest instructions

--- pipeline_tests/pretest_prep.sh
+++ pipeline_tests/pretest_prep.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+apt update --option Acquire::HTTPS::Proxy=https://contractorproxyeast.northgrum.com:80
+
 installed_py_ver=$(python3 --version)
 if [[ $installed_py_ver != *"3.8."* ]] ; then
-  - apt update --option Acquire::HTTPS::Proxy=https://contractorproxyeast.northgrum.com:80
-  - apt-get -y install python3 virtualenv flake8
-  virtualenv --python=python3.8 /home/sleipnir_pipeline/py3.8
-  pip install pytest coverage
-else
-  virtualenv --python=python3.8 /home/sleipnir_pipeline/py3.8
+  apt-get -y install python3
 fi
+
+apt-get -y install virtualenv
+virtualenv --python=python3.8 /home/sleipnir_pipeline/py3.8
+apt-get -y install flake8
+pip install pytest coverage
