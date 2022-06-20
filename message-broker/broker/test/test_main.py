490840e669284420fa40182d62c6cb013713c5d1
n08066 <Matthew.Hendrickson2@ngc.com>
2022-06-20 14:42:03 -0400

    Testing what happens with basic python file to test

---
+++ message-broker/broker/test/test_main.py
@@ -0,0 +1,4 @@
+from src.main import main
+
+def test_main() -> None:
+    assert main() is None
--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 echo "Starting unit tests for MessageBroker project..."
-pytest message-broker/broker/test/
-pytest message-broker/testing-framework/test/
+python -m pytest --cov-report=html --cov=src message-broker/broker/
+#pytest message-broker/broker/test/
+#pytest message-broker/testing-framework/test/
