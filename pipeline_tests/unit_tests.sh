ac2c8144c778be47f3c702ec3c691e16b5c6a00e
n08066 <Matthew.Hendrickson2@ngc.com>
2022-06-20 14:55:24 -0400

    Adjusting paths

--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 echo "Starting unit tests for MessageBroker project..."
-python -m pytest --cov-report=html --cov=src message-broker/broker/
+cd message-broker/broker
+python -m pytest --cov-report=html --cov=src .
+cd -
 #pytest message-broker/broker/test/
 #pytest message-broker/testing-framework/test/
