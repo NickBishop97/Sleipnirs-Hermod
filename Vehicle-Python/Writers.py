bd1f17242c83594790264e7f9389db727804b792
n08066 <Matthew.Hendrickson2@ngc.com>
2022-06-28 17:29:31 -0400

    Finished entity.py and distancedis.py

--- Vehicle-Python/Writers.py
+++ Vehicle-Python/Writers.py
@@ -1,4 +1,4 @@
-from threading import Condition
+# from threading import Condition
 import time
 import random
 
@@ -10,71 +10,71 @@ from entity import Entity
 ############################################################################################
 ############################################################################################
 
+
 class FuelWriter(Entity.Writer):
     def __init__(self, myPubSubType, myPubSubType_name, myTopic_name):
         self.MessageType = myPubSubType
         self.MessageType_name = myPubSubType_name
         self.Topic_name = myTopic_name
-        
+
         self.total_fuel = 100.0
         self.fuel_change_rate = 0
         super().__init__(myPubSubType, myPubSubType_name, myTopic_name)
-        
+
     def write(self):
         if self.total_fuel <= 0:
             self.fuel_change_rate = 0
             self.total_fuel = 0
-            
-        #UPDATING MESSAGE CONTENTS
+
+        # UPDATING MESSAGE CONTENTS
         self.data.message(str(round(self.fuel_change_rate, 1)) + ", " + str(round(self.total_fuel, 1)))
         self.data.index(self.index)
-        
+
         self.writer.write(self.data)
         print("{index}, {message}\n".format(message=self.data.message(), index=self.data.index()))
         self.index = self.index + 1
 
-        #WRITE STATE UPDATE
+        # WRITE STATE UPDATE
         if self.total_fuel > 0:
             self.fuel_change_rate = random.uniform(0.01, 0.2)
             self.total_fuel -= self.fuel_change_rate
 
-        
     def run(self):
         self.wait_discovery()
         while True:
             self.write()
             time.sleep(0.25)
-        
+
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
 
 class MilesWriter(Entity.Writer):
     def __init__(self, myPubSubType, myPubSubType_name, myTopic_name):
         self.MessageType = myPubSubType
         self.MessageType_name = myPubSubType_name
         self.Topic_name = myTopic_name
-        
+
         self.milesTraveled = 0.0
         super().__init__(myPubSubType, myPubSubType_name, myTopic_name)
 
     def write(self, stopMoving):
-        #UPDATING MESSAGE CONTENTS
+        # UPDATING MESSAGE CONTENTS
         self.data.message(str(self.milesTraveled))
         self.data.index(self.index)
         self.writer.write(self.data)
         print("{index}: {message}\n".format(index=self.data.index(), message=self.data.message()))
         self.index += 1
-        
-        #WRITE STATE UPDATE
+
+        # WRITE STATE UPDATE
         if stopMoving.milesStopper:
             self.milesTraveled += 0
         else:
             self.milesTraveled += random.uniform(1, 2)
-            
-        
+
     def run(self, stopMoving):
         self.wait_discovery()
         while True:
@@ -88,4 +88,3 @@ class MilesWriter(Entity.Writer):
 ############################################################################################
 ############################################################################################
 ############################################################################################
-
