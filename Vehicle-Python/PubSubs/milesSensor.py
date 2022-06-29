e498a6e344d06e95ec7bcbc3a2a9cbd40a8edcc3
n13853 <Maxwell.Rosales@ngc.com>
2022-06-29 13:35:52 -0400

    Started work on calculators and cleaning up constructors

---
+++ Vehicle-Python/PubSubs/milesSensor.py
@@ -0,0 +1,70 @@
+from threading import Thread
+import signal
+import time
+import sys
+
+# IDL DATA IMPORTS
+sys.path.insert(0, '../MessageFormats/Fuel/')
+import Fuel as Fuel
+sys.path.insert(1, '../MessageFormats/Miles/')
+import Miles as Miles  
+
+#ADT IMPORTS
+sys.path.insert(2, '../ADTs/')
+from Writers import *  
+from Readers import *
+from Calculators import *
+
+
+def calc(fuelQueue, milesStopper):
+    while True:
+        if not fuelQueue.empty():
+            print(f"Fuel Data: {fuelQueue.get()}")
+        if float(fuelQueue.get()) <= 0:
+            milesStopper.milesStopper = True
+
+
+class MilesStopper:
+    milesStopper = False
+
+
+def main():
+    writers = []
+    readers = []
+    threads = []
+    signal.signal(signal.SIGINT,
+                  lambda sig, frame: (
+                    print("\nStopped!"),
+                    [reader.delete() for reader in readers],
+                    [writer.delete() for writer in writers],
+                    sys.exit(0),
+                  ))
+
+    print("Press Ctrl+C to stop")
+    milesStopper = MilesStopper()
+    FuelReader = FuelGauge([Fuel, "Fuel", "FuelRemaining", FuelRL])  # noqa: F405
+    DistWriter = MilesWriter([Miles, "Miles", "MilesTraveled"])  # noqa: F405
+
+    readers.append(FuelReader)
+    writers.append(DistWriter)
+
+    # Add readers and start threads
+    FuelThread = Thread(target=(FuelReader.run), daemon=True)
+    DistThread = Thread(target=(DistWriter.run), args=(milesStopper,), daemon=True)
+    CalcThread = Thread(target=(calc),
+                        args=(
+                            readers[0].dataQueue,
+                            milesStopper,),
+                        daemon=True)
+
+    threads.append(FuelThread)
+    threads.append(DistThread)
+    threads.append(CalcThread)
+
+    for thread in threads:
+        thread.start()
+
+    signal.pause()
+
+
+main()
