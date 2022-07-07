c957113fab6173dbe3aa54f2001174a9276a479c
n16743 <nicholas.bishop2@ngc.com>
2022-07-06 23:23:59 -0400

    Created MilesTraveled, and Moving Topics

--- Vehicle-C++/Vehicle/src/Calculations.h
+++ Vehicle-C++/Vehicle/src/Calculations.h
@@ -6,23 +6,47 @@
 #include <vector>
 #include <iostream>
 
+/**
+ * @brief Enables communication between pub/sub in FuelSensor.h and does calculations that are required for functionality 
+ * 
+ */
 class FuelSenor
 {
 
 private:
     unsigned long index = 0;
+    unsigned long check = 1;
     double FuelSpent = 0.0;
-    double FuelRemaining = 0.0;
+    double FuelRemaining = 10.0;
 
 public:
     double fuelspent(double fuelR);
     double get_FuelRemaining();
     unsigned long get_index();
+    unsigned long get_check();
     void set_FuelRemaining(double fuelR);
     void set_index(unsigned long i);
+    void set_check(unsigned long c);
 };
 
+/**
+ * @brief Enables communication between pub/sub in Moving.h
+ * 
+ */
 class Moving
+{
+    private:
+        unsigned long index;
+    public:
+        unsigned long get_index();
+        void set_index(unsigned long i);
+};
+
+/**
+ * @brief Enables communication between pub/sub in MilesTraveled.h
+ * 
+ */
+class MilesTraveled
 {
     private:
         unsigned long index;
