8230498cb01b3c50af66f6daa4633e7b512d135b
Spencer Williams <Spencer.Williams3@ngc.com>
2022-07-12 13:41:05 -0400

    Editing Readers and Writers

--- Vehicle-Python/ADTs/Readers.py
+++ Vehicle-Python/ADTs/Readers.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread    
 from entity import Entity
 import fastdds
 from queue import Queue
@@ -15,11 +15,10 @@ from queue import Queue
 ############################################################################################
 ############################################################################################
 
-
 class FuelGauge(Entity.Reader):
     def __init__(self, ddsDataArray):
         super().__init__(ddsDataArray)
-
+        
         self.connected = self.listener.getConnectionStatus()
 
     def dataRunReturn(self):
@@ -27,7 +26,7 @@ class FuelGauge(Entity.Reader):
             if not self.dataQueue.empty():
                 self.dataQueue = self.listener.getDataReturn()
                 self.connected = self.listener.getConnectionStatus()
-                # return (self.dataQueue, self.connected)
+                #return (self.dataQueue, self.connected)
             else:
                 return None
 
@@ -35,18 +34,17 @@ class FuelGauge(Entity.Reader):
         dataThread = Thread(target=(self.dataRunReturn), daemon=True)
         dataThread.start()
 
-
 class FuelRL(Entity.ReaderListener):
     def __init__(self, data):
         self.data = data
         self.dataQueue = Queue()
-
+    
         class ConnectionStatus:
             def __init__(self):
                 self.connected = False
-
+            
         self.connectionStatus = ConnectionStatus()
-
+        
         super().__init__()
 
     def on_subscription_matched(self, datareader, info):
@@ -56,20 +54,20 @@ class FuelRL(Entity.ReaderListener):
             print("Subscriber unmatched publisher {}".format(info.last_publication_handle))
 
     def on_data_available(self, reader):
-
+        
         self.connectionStatus.connected = True
-
+        
         info = fastdds.SampleInfo()
         data = self.data
         reader.take_next_sample(data, info)
-
-        self.dataQueue.put([data.index(),
+        #print(f"[FUEL: REMAINING, SPENT]: {data.index()}, {data.litersRemaining()}, {data.litersSpent()}")
+        self.dataQueue.put([data.index(), 
                             data.litersRemaining(),
                             data.litersSpent()])
 
     def getConnectionStatus(self):
         return self.connectionStatus
-
+    
     def getDataReturn(self):
         return self.dataQueue
 
@@ -79,11 +77,10 @@ class FuelRL(Entity.ReaderListener):
 ############################################################################################
 ############################################################################################
 
-
 class DistanceDisplay(Entity.Reader):
     def __init__(self, ddsDataArray):
         super().__init__(ddsDataArray)
-
+    
     def dataRunReturn(self):
         while True:
             if not self.dataQueue.empty():
@@ -94,7 +91,6 @@ class DistanceDisplay(Entity.Reader):
     def run(self):
         dataThread = Thread(target=(self.dataRunReturn), daemon=True)
         dataThread.start()
-
 
 class DistanceRL(Entity.ReaderListener):
     def __init__(self, data):
@@ -114,8 +110,139 @@ class DistanceRL(Entity.ReaderListener):
         data = self.data
         reader.take_next_sample(data, info)
         milesTraveled = float(data.milesTraveled())
-        # print(f"MILES READ: {milesTraveled}")
-        self.dataQueue.put(milesTraveled)
+        #print(f"[DISTANCE TRAVELED]: {data.index()}, {milesTraveled}")
+        self.dataQueue.put([data.index(),
+                            data.milesTraveled()])
 
     def getDataReturn(self):
         return self.dataQueue
+
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+
+class MpGDisplay(Entity.Reader):
+    def __init__(self, ddsDataArray):
+        super().__init__(ddsDataArray)
+    
+    def dataRunReturn(self):
+        while True:
+            if not self.dataQueue.empty():
+                self.dataQueue = self.listener.getDataReturn()
+            else:
+                return None
+
+    def run(self):
+        dataThread = Thread(target=(self.dataRunReturn), daemon=True)
+        dataThread.start()
+
+class MpGRL(Entity.ReaderListener):
+    def __init__(self, data):
+        self.data = data
+        self.dataQueue = Queue()
+        super().__init__()
+
+    def on_subscription_matched(self, datareader, info):
+        if(0 < info.current_count_change):
+            print("Subscriber matched publisher {}".format(info.last_publication_handle))
+        else:
+            print("Subscriber unmatched publisher {}".format(info.last_publication_handle))
+            # exit()
+
+    def on_data_available(self, reader):
+        info = fastdds.SampleInfo()
+        data = self.data
+        reader.take_next_sample(data, info)
+        #print(f"[MPG]: {data.index()}, {float(data.mpg())}")
+        self.dataQueue.put([data.index(),
+                            float(data.mpg())])
+
+    def getDataReturn(self):
+        return self.dataQueue
+
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+
+class LowFuelAlertDisplay(Entity.Reader):
+    def __init__(self, ddsDataArray):
+        super().__init__(ddsDataArray)
+    
+    def dataRunReturn(self):
+        while True:
+            if not self.dataQueue.empty():
+                self.dataQueue = self.listener.getDataReturn()
+            else:
+                return None
+
+    def run(self):
+        dataThread = Thread(target=(self.dataRunReturn), daemon=True)
+        dataThread.start()
+
+class LowFuelAlertRL(Entity.ReaderListener):
+    def __init__(self, data):
+        self.data = data
+        self.dataQueue = Queue()
+        super().__init__()
+
+    def on_subscription_matched(self, datareader, info):
+        if(0 < info.current_count_change):
+            print("Subscriber matched publisher {}".format(info.last_publication_handle))
+        else:
+            print("Subscriber unmatched publisher {}".format(info.last_publication_handle))
+            # exit()
+
+    def on_data_available(self, reader):
+        info = fastdds.SampleInfo()
+        data = self.data
+        reader.take_next_sample(data, info)
+        
+        #print(f"[LOW FUEL]: {data.index()}, {bool(data.isFuelLow())}")
+        self.dataQueue.put([data.index(),
+                            bool(data.isFuelLow())])
+
+    def getDataReturn(self):
+        return self.dataQueue
+    
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+class MilesRemainDisplay(Entity.Reader):
+    def __init__(self, ddsDataArray):
+        super().__init__(ddsDataArray)
+    
+    def dataRunReturn(self):
+        while True:
+            if not self.dataQueue.empty():
+                self.dataQueue = self.listener.getDataReturn()
+            else:
+                return None
+
+    def run(self):
+        dataThread = Thread(target=(self.dataRunReturn), daemon=True)
+        dataThread.start()
+
+class MilesRemainRL(Entity.ReaderListener):
+    def __init__(self, data):
+        self.data = data
+        self.dataQueue = Queue()
+        super().__init__()
+
+    def on_data_available(self, reader):
+        info = fastdds.SampleInfo()
+        data = self.data
+        reader.take_next_sample(data, info)
+        
+        #print(f"[MILES REMAIN]: {data.index()}, {data.milesToRefuel()}")
+        self.dataQueue.put([data.index(),
+                            data.milesToRefuel()])
+
+    def getDataReturn(self):
+        return self.dataQueue
+    
--- Vehicle-Python/ADTs/Writers.py
+++ Vehicle-Python/ADTs/Writers.py
@@ -1,30 +1,28 @@
 # from threading import Condition
 import time
-# import random
+import random
 
-# ADT IMPORTS
-from entity import Entity
-from Calculators import *  # noqa: F403,F401
+#ADT IMPORTS
+from entity import Entity  
+from Calculators import *
 
+global_sleep_time          = float(0.25)
+global_sleep_time_buffered = global_sleep_time + float(0.03)
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
-
-
 class FuelWriter(Entity.Writer):
-    # def __init__(self, ddsDataArray, FuelConsump):
     def __init__(self, ddsDataArray, CalculationClass):
 
         self.CalculationClass = CalculationClass
         super().__init__(ddsDataArray)
-        self.CalculationClass = CalculationClass
 
-    def write(self, test_flag):
-
+    def write(self, stop_flag):
+        
         # UPDATING MESSAGE CONTENTS
-        dataOutput = self.CalculationClass.consumeFuel(test_flag)
+        dataOutput = self.CalculationClass.consumeFuel(stop_flag)
         self.data.litersRemaining(float(dataOutput[0]))
         self.data.litersSpent(float(dataOutput[1]))
         self.data.index(self.index)
@@ -33,24 +31,21 @@ class FuelWriter(Entity.Writer):
         print(f"{self.index}, {dataOutput[0]}, {dataOutput[1]} \n")
         self.index = self.index + 1
 
-    def run(self, test_flag) -> None:
-        # self.wait_discovery()
+    def run(self, stop_flag) -> None: 
+        #self.wait_discovery()
         while True:
-            self.write(test_flag)
-            time.sleep(0.25)
+            self.write(stop_flag)
+            time.sleep(global_sleep_time)
 
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
-
-
 class MilesWriter(Entity.Writer):
     def __init__(self, ddsDataArray, CalculationClass):
         self.CalculationClass = CalculationClass
         super().__init__(ddsDataArray)
-        self.CalculationClass = CalculationClass
 
     def write(self, startStopCondition):
         # UPDATING MESSAGE CONTENTS
@@ -58,15 +53,15 @@ class MilesWriter(Entity.Writer):
         self.data.milesTraveled(float(self.CalculationClass.milesTraveled))
         self.data.index(self.index)
         self.writer.write(self.data)
-
+        
         print(f"MILES TRAVELED: {self.data.index()}, {self.data.milesTraveled()}\n")
         self.index += 1
 
     def run(self, startStopCondition):
-        # self.wait_discovery()
+        #self.wait_discovery()
         while True:
             self.write(startStopCondition)
-            time.sleep(0.25)    # Report every 0.25 s
+            time.sleep(global_sleep_time)    # Report every 0.25 s
             # 25 MPH = 0.001 miles in 0.25 s
             # 85 MPH = 0.006 miles in 0.25 s
 
@@ -76,66 +71,88 @@ class MilesWriter(Entity.Writer):
 ############################################################################################
 ############################################################################################
 
-
 class LowFuelWriter(Entity.Writer):
     def __init__(self, ddsDataArray):
         super().__init__(ddsDataArray)
 
-    def write(self, alertStatus):
-
+    def write(self, fuelQueue):
+        lowFuel = LowFuelCalc(50)
+        alert = lowFuel.lowFuelAlert(fuelQueue.get()[1])
         # UPDATING MESSAGE CONTENTS
-        self.data.message(f"{alertStatus}")
+        self.data.isFuelLow(alert)
         self.data.index(self.index)
-
         self.writer.write(self.data)
-        # print("{index}, {message}\n".format(message=self.data.message(), index=self.data.index()))
+        print(f"Low Fuel: {self.data.index()}, {bool(self.data.isFuelLow())}\n")
+            
         self.index = self.index + 1
 
-    def run(self, alertStatus):
-        # self.wait_discovery()
+    def run(self, fuelQueue):
+        #self.wait_discovery()
         while True:
-            self.write(alertStatus)
-            time.sleep(0.25)
-
+            self.write(fuelQueue)
+            time.sleep(global_sleep_time)
+            
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
-
 
 class MpGWriter(Entity.Writer):
-    def __init__(self, ddsDataArray):  # CalculationClass):
+    def __init__(self, ddsDataArray): #, CalculationClass):
 
-        # self.CalculationClass = CalculationClass
+        #self.CalculationClass = CalculationClass
         super().__init__(ddsDataArray)
 
     def write(self, fuelQueue, milesQueue):
-        fuelDatum = 0
-        milesDatum = 0
+        mpgCalc = MpGCalc()
 
-        if not fuelQueue.empty():
-            fuelDatum = fuelQueue.get()[2]
-
-        if not milesQueue.empty():
-            milesDatum = milesQueue.get()
-
-        if not fuelDatum == 0:
-            temp = float(milesDatum)/float(fuelDatum)
-
-            self.data.index(self.index)
-            self.data.mpg(temp)
-            print(f"MpG: {self.index}, {temp} \n")
-
-        elif fuelDatum == 0:
-            self.data.index(self.index)
-            self.data.mpg(float(-1))
-            print(f"MpG: {self.index}, {-1} \n")
-
+        self.data.index(self.index)
+        self.data.mpg(mpgCalc.calculateMpG(fuelQueue, milesQueue))
+        
+        self.writer.write(self.data)
+        
+        print(f"MpG: {self.data.index()}, {self.data.mpg()} \n")
         self.index = self.index + 1
-
+            
+            
     def run(self, fuelQueue, milesQueue):
-        # self.wait_discovery()
+        #self.wait_discovery()
         while True:
             self.write(fuelQueue, milesQueue)
-            time.sleep(0.3)
+            time.sleep(global_sleep_time_buffered)
+  
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+############################################################################################
+
+class MilesRemaining(Entity.Writer):
+    def __init__(self, ddsDataArray): #, CalculationClass):
+
+        #self.CalculationClass = CalculationClass
+        super().__init__(ddsDataArray)
+
+    def write(self, fuelQueue, mpgQueue):
+
+        milesRemainCalc = MileRemainCalc()
+        
+        self.data.index(self.index)
+        self.data.milesToRefuel(milesRemainCalc.calculateMpG(fuelQueue, mpgQueue))
+        self.writer.write(self.data)
+        
+        if not fuelQueue.empty() or not mpgQueue.empty():
+            print(f"MilesToRefuel: {self.data.index()}, {self.data.milesToRefuel()} \n")
+        else:
+            print("MilesToRefuel:")
+            
+        self.index = self.index + 1
+            
+            
+    def run(self, fuelQueue, mpgQueue):
+        #self.wait_discovery()
+        while True:
+            self.write(fuelQueue, mpgQueue)
+            time.sleep(global_sleep_time_buffered)
+
