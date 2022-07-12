386acbcca306da95670d6b033c4ff53932552663
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-12 12:07:42 -0400

    Adding C++ linting

--- pipeline_tests/lint_tests.sh
+++ pipeline_tests/lint_tests.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
-echo "Starting lint on Python section of Vehicle Project..."
-flake8 Vehicle-Python --max-line-length 130
+echo "====================================="
+echo "=  Starting lint on Vehicle-Python  ="
+echo "====================================="
+#flake8 Vehicle-Python --max-line-length 130
+
+echo "=================================="
+echo "=  Starting lint on Vehicle-C++  ="
+echo "=================================="
+cd Vehicle-C++/Vehicle
+sh cxxlint_test.sh ./src/
+cd -
