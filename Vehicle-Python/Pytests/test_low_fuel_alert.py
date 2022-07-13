597692e4720b714455032fd292e3329b8d4042f9
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 12:56:36 -0400

    Fixed a couple pytests

--- Vehicle-Python/Pytests/test_low_fuel_alert.py
+++ Vehicle-Python/Pytests/test_low_fuel_alert.py
@@ -1,46 +1,46 @@
 import sys
 
 sys.path.insert(0, "../ADTs")
-from Calculators import LowFuel  # noqa E402 (linting exemption)
+from Calculators import LowFuelCalc  # noqa E402 (linting exemption)
 
 
 # There is enough fuel
 def test_enough_fuel():
-    f = LowFuel(5)
+    f = LowFuelCalc(5)
     f.lowFuelAlert(10)
     assert f.lowFuelAlertFlag == 0
 
 
 # Fuel is low
 def test_low_fuel():
-    f = LowFuel(5)
+    f = LowFuelCalc(5)
     f.lowFuelAlert(2)
     assert f.lowFuelAlertFlag == 1
 
 
 # Fuel is at threshold but not low
 def test_on_mark_fuel():
-    f = LowFuel(5)
+    f = LowFuelCalc(5)
     f.lowFuelAlert(5)
     assert f.lowFuelAlertFlag == 0
 
 
 # Fuel is empty
 def test_zero_fuel():
-    f = LowFuel(5)
+    f = LowFuelCalc(5)
     f.lowFuelAlert(0)
     assert f.lowFuelAlertFlag == 1
 
 
 # Fuel is negative should result an error code of -1
 def test_negative_fuel():
-    f = LowFuel(5)
+    f = LowFuelCalc(5)
     f.lowFuelAlert(-1.0)
     assert f.lowFuelAlertFlag == -1
 
 
 # Fuel capacity is negative, then send an error
 def test_negative_capacity():
-    f = LowFuel(-6)
+    f = LowFuelCalc(-6)
     f.lowFuelAlert(20)
     assert f.lowFuelAlertFlag == -1
--- Vehicle-Python/Pytests/test_mpg_monitor.py
+++ Vehicle-Python/Pytests/test_mpg_monitor.py
@@ -2,34 +2,34 @@
 import sys
 
 sys.path.insert(1, '../ADTs/')
-from Calculators import MPG  # noqa E402 (linting exemption)
+from Calculators import MpGCalc  # noqa E402 (linting exemption)
 
 
 def testNewTank():
-    CalculatorClass = MPG(100, 0, 100)
+    CalculatorClass = MpGCalc(100, 0, 100)
     assert CalculatorClass.getMPG() == -1, "MPG did not return error code when tank full and no distance"
 
 
 def testInvalidTank():
-    CalculatorClass = MPG(120, 0, 100)
+    CalculatorClass = MpGCalc(120, 0, 100)
     assert CalculatorClass.getMPG() == -1, "MPG did not return error code when negative fuel spent"
 
 
 def testStdTank():
-    CalculatorClass = MPG(80, 120, 100)
+    CalculatorClass = MpGCalc(80, 120, 100)
     assert CalculatorClass.getMPG() == 6, "MPG not correctly calculated for standard numbers"
 
 
 def testLargeTank():
-    CalculatorClass = MPG(5000, 200000, 10000)
+    CalculatorClass = MpGCalc(5000, 200000, 10000)
     assert CalculatorClass.getMPG() == 40, "MPG not correctly calculated for large numbers"
 
 
 def testEmptyTank():
-    CalculatorClass = MPG(0, 1000, 100)
+    CalculatorClass = MpGCalc(0, 1000, 100)
     assert CalculatorClass.getMPG() == 10, "MPG not correctly calculated for empty tank"
 
 
 def testOverdrawnTank():
-    CalculatorClass = MPG(-1, 1010, 100)
+    CalculatorClass = MpGCalc(-1, 1010, 100)
     assert CalculatorClass.getMPG() == 10, "MPG not correctly calculated for empty tank"
