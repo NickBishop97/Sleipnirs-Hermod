d43c2bbd0d9a70c92b88de90dceeda0486cbc9ce
Spencer Williams <Spencer.Williams3@ngc.com>
2022-07-13 17:45:44 -0400

    Starting pytest for trip

---
+++ Examples/Trip_Meter/N16743/N13823/test_trip_meter.py
@@ -0,0 +1,27 @@
+from sleipnir.Examples.Trip_Meter.N16743.N13823.TripMeter import TripMeter
+
+# Verify that inputed/current tripData displays properly
+# for trip #1
+def test_trip1_input():
+    print("stuff")
+
+# Verify that inputed/current tripData displays properly
+# for trip #2
+def test_trip2_input():
+    print("stuff")
+
+# Verify that switching from trip1 to trip2 works
+def test_trip1_switchTo2():
+    print("stuff")
+
+# Verify that switching from trip2 to trip1 works
+def test_trip2_switchTo1():
+    print("stuff")
+
+# Verify that trip1 data resets to initial values
+def test_trip1_reset():
+    print("stuff")
+
+# Verify that trip2 data resets to initial values
+def test_trip2_reset():
+    print("stuff")
