05b5dfcd3fcf2308d7f9b7676e3ea3cdc4c27e5b
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 13:01:18 -0400

    Updated pipeline to exclude autogenerated dirs

--- pipeline_tests/lint_tests.sh
+++ pipeline_tests/lint_tests.sh
@@ -3,7 +3,11 @@
 echo "====================================="
 echo "=  Starting lint on Vehicle-Python  ="
 echo "====================================="
-#flake8 Vehicle-Python --max-line-length 130
+flake8 Vehicle-Python/ADTs --max-line-length 130
+flake8 Vehicle-Python/Publishers --max-line-length 130
+flake8 Vehicle-Python/PubSubs --max-line-length 130
+flake8 Vehicle-Python/Pytests --max-line-length 130
+flake8 Vehicle-Python/Subscribers --max-line-length 130
 
 echo "=================================="
 echo "=  Starting lint on Vehicle-C++  ="
