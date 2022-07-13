cda9e84baa05c0832801bc90e5a0a7c7114ee494
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 12:44:57 -0400

    Linted dashboard.py

--- Vehicle-Python/Subscribers/dashboard.py
+++ Vehicle-Python/Subscribers/dashboard.py
@@ -3,23 +3,31 @@ import signal
 import time
 import sys
 
+# Flask Imports
+import json
+import random
+# from datetime import datetime
+
+from flask import Flask, Response, render_template, stream_with_context
+
 # IDL DATA IMPORTS
 sys.path.insert(0, '../MessageFormats/Fuel/')
-import Fuel as Fuel
+import Fuel as Fuel  # noqa E402 (linting exemption)
 sys.path.insert(1, '../MessageFormats/Miles/')
-import Miles as Miles  
+import Miles as Miles  # noqa E402 (linting exemption)
 sys.path.insert(2, '../MessageFormats/MpG/')
-import MpG as MpG  
+import MpG as MpG  # noqa E402 (linting exemption)
 sys.path.insert(3, '../MessageFormats/LowFuelAlert/')
-import LowFuelAlert as LowFuelAlert  
+import LowFuelAlert as LowFuelAlert  # noqa E402 (linting exemption)
 sys.path.insert(4, '../MessageFormats/MilesToRefuel/')
-import MilesToRefuel as MilesToRefuel  
+import MilesToRefuel as MilesToRefuel  # noqa E402 (linting exemption)
 
-#ADT IMPORTS
+# ADT IMPORTS
 sys.path.insert(5, '../ADTs/')
-from Writers import *  
-from Readers import *
-from Calculators import *
+from Writers import *  # noqa E402,F403 (linting exemptions)
+from Readers import *  # noqa E402,F403 (linting exemptions)
+from Calculators import *  # noqa E402,F403 (linting exemptions)
+
 
 #####################################################
 #####################################################
@@ -28,17 +36,19 @@ from Calculators import *
 #####################################################
 #####################################################
 #####################################################
+
 
 def printer(queueList):
     while True:
-        #if not queueList[0].empty():
+        # if not queueList[0].empty():
         print(f"FUEL        :{queueList[0].get()}")
         print(f"MILES       :{queueList[1].get()}")
         print(f"MPG         :{queueList[2].get()}")
         print(f"LOWFUEL     :{queueList[3].get()}")
         print(f"MILESREMAIN :{queueList[4].get()}")
         print("\n\n")
-        time.sleep(0.3)  
+        time.sleep(0.3)
+
 
 def main():
     readers = []
@@ -52,33 +62,25 @@ def main():
 
     print("Press Ctrl+C to stop")
 
-    readers.append(FuelGauge([Fuel, "Fuel", "FuelRemaining544645", FuelRL]))
-    readers.append(DistanceDisplay([Miles, "Miles", "MilesTraveled", DistanceRL]))
-    readers.append(MpGDisplay([MpG, "MpG", "MpGCumulative", MpGRL]))
-    readers.append(LowFuelAlertDisplay([LowFuelAlert, "LowFuelAlert", "LowFuelAlert", LowFuelAlertRL]))
-    readers.append(MilesRemainDisplay([MilesToRefuel, "MilesToRefuel", "MilesToRefuelTopic", MilesRemainRL]))
-    
+    readers.append(FuelGauge([Fuel, "Fuel", "FuelRemaining544645", FuelRL]))  # noqa F405 (linting exemption)
+    readers.append(DistanceDisplay([Miles, "Miles", "MilesTraveled", DistanceRL]))  # noqa F405 (linting exemption)
+    readers.append(MpGDisplay([MpG, "MpG", "MpGCumulative", MpGRL]))  # noqa F405 (linting exemption)
+    readers.append(LowFuelAlertDisplay([LowFuelAlert, "LowFuelAlert", "LowFuelAlert", LowFuelAlertRL]))  # noqa F405
+    readers.append(MilesRemainDisplay([MilesToRefuel, "MilesToRefuel", "MilesToRefuelTopic", MilesRemainRL])) # noqa F405
+
     for reader in readers:
         threads.append(Thread(target=(reader.run), daemon=True))
     threads.append(Thread(target=(printer),
-                          args=([reader.dataQueue for reader in readers],), 
+                          args=([reader.dataQueue for reader in readers],),
                           daemon=True))
-    
+
     for thread in threads:
         thread.start()
-    
+
     signal.pause()
 
 
-#main()
-
-
-import json
-import random
-import time
-from datetime import datetime
-
-from flask import Flask, Response, render_template, stream_with_context
+# main()
 
 application = Flask(__name__)
 random.seed()  # Initialize the random number generator
@@ -96,9 +98,9 @@ def chart_data():
         while True:
             json_data = json.dumps(
                 {
-                'index': readers[2].dataQueue.get()[0], 
-                'mpg': readers[2].dataQueue.get()[1],
-                 })
+                    'index': readers[2].dataQueue.get()[0],
+                    'mpg': readers[2].dataQueue.get()[1],
+                })
             yield f"data:{json_data}\n\n"
             time.sleep(0.25)
 
@@ -112,24 +114,24 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT,
                   lambda sig, frame: (
                     print("\nStopped!"),
-                    #[reader.delete() for reader in readers],
+                    # [reader.delete() for reader in readers],
                     sys.exit(0),
                   ))
 
     print("Press Ctrl+C to stop")
-    
+
     readers = []
     threads = []
-    readers.append(FuelGauge([Fuel, "Fuel", "FuelRemaining544645", FuelRL]))
-    readers.append(DistanceDisplay([Miles, "Miles", "MilesTraveled", DistanceRL]))
-    readers.append(MpGDisplay([MpG, "MpG", "MpGCumulative", MpGRL]))
-    readers.append(LowFuelAlertDisplay([LowFuelAlert, "LowFuelAlert", "LowFuelAlert", LowFuelAlertRL]))
-    readers.append(MilesRemainDisplay([MilesToRefuel, "MilesToRefuel", "MilesToRefuelTopic", MilesRemainRL]))
+    readers.append(FuelGauge([Fuel, "Fuel", "FuelRemaining544645", FuelRL])) # noqa F405 (linting exemption)
+    readers.append(DistanceDisplay([Miles, "Miles", "MilesTraveled", DistanceRL]))  # noqa F405
+    readers.append(MpGDisplay([MpG, "MpG", "MpGCumulative", MpGRL]))  # noqa F405 (linting exemption)
+    readers.append(LowFuelAlertDisplay([LowFuelAlert, "LowFuelAlert", "LowFuelAlert", LowFuelAlertRL]))  # noqa F405
+    readers.append(MilesRemainDisplay([MilesToRefuel, "MilesToRefuel", "MilesToRefuelTopic", MilesRemainRL]))  # noqa F405
     for reader in readers:
         threads.append(Thread(target=(reader.run), daemon=True))
-    
+
     for thread in threads:
         thread.start()
-        
+
     application.run(debug=True, threaded=True)
     signal.pause()
