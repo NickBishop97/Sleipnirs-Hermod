056f3e8676ccf0a19336424254a3039a5b96548d
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 12:22:12 -0400

    Linted Test_Fuel_Guague.py

--- Vehicle-Python/Pytests/test_fuel_gauge.py
+++ Vehicle-Python/Pytests/test_fuel_gauge.py
@@ -1,7 +1,6 @@
 import sys
 sys.path.insert(0, "../ADTs")
-from Calculators import Fuelgauge
-
+from Calculators import Fuelgauge  # noqa E402 (linting exemption)
 
 
 def test_base_case():
@@ -10,11 +9,13 @@ def test_base_case():
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 50.0
 
+
 def test_zero_over_full():
     test_gauge = Fuelgauge(0.0, 100.0)
     print("Expected: 0.0")
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 0.0
+
 
 def test_zero_over_zero():
     test_gauge = Fuelgauge(0.0, 0.0)
@@ -22,11 +23,13 @@ def test_zero_over_zero():
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 0.0
 
+
 def test_negative_over_full():
     test_gauge = Fuelgauge(-50.0, 100.0)
     print("Expected: 0.0")
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 0.0
+
 
 def test_side_case():
     test_gauge = Fuelgauge(129.0, 150.0)
@@ -34,11 +37,13 @@ def test_side_case():
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 86.0
 
+
 def test_half_over_zero():
     test_gauge = Fuelgauge(50.0, 0.0)
     print("Expected: 0.0")
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 0.0
+
 
 def test_extra_over_full():
     test_gauge = Fuelgauge(150.0, 100.0)
@@ -46,17 +51,20 @@ def test_extra_over_full():
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 100.0
 
+
 def test_full_over_full():
     test_gauge = Fuelgauge(100.0, 100.0)
     print("Expected: 100.0")
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 100.0
 
+
 def test_full_over_negative():
     test_gauge = Fuelgauge(100.0, -50.0)
     print("Expected: 0.0")
     print("Actual Result: " + str(test_gauge.getRemainingFuel()))
     assert test_gauge.getRemainingFuel() == 0.0
+
 
 test_base_case()
 print('')
