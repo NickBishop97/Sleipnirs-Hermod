1093f6b38682bf9e82b82fb7b87755b0238c0bc9
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 12:13:47 -0400

    Linted milesSensor.py

--- Vehicle-Python/PubSubs/milesSensor.py
+++ Vehicle-Python/PubSubs/milesSensor.py
@@ -1,39 +1,40 @@
 from threading import Thread
 import signal
-import time
+# import time
 import sys
 
 # IDL DATA IMPORTS
 sys.path.insert(0, '../MessageFormats/Fuel/')
-import Fuel as Fuel
+import Fuel as Fuel  # noqa E402 (linting exemption)
 sys.path.insert(1, '../MessageFormats/Miles/')
-import Miles as Miles  
+import Miles as Miles  # noqa E402 (linting exemption)
 
-#ADT IMPORTS
+# ADT IMPORTS
 sys.path.insert(2, '../ADTs/')
-from Writers import *  
-from Readers import *
-from Calculators import *
+from Writers import *  # noqa E402,F403 (linting exemptions)
+from Readers import *  # noqa E402,F403 (linting exemptions)
+from Calculators import *  # noqa E402,F403 (linting exemptions)
 
 
 def fuelConnectionStatus(dataQueue, connected, startStopCondition):
     while True:
-        
-        #print(f"Connected? {str(connected.connected)}")
+
+        # print(f"Connected? {str(connected.connected)}")
         if not dataQueue.empty():
             data = dataQueue.get()
             startStopCondition.milesStarter = True
-        
+
             if float(data[1]) <= 0:
                 startStopCondition.milesStopper = True
                 print("*****NO FUEL*****")
-                
+
             print("\n")
-                    
+
 
 class StartStopCondition:
     milesStarter = False
     milesStopper = False
+
 
 def main():
     writers = []
@@ -49,8 +50,8 @@ def main():
 
     print("Press Ctrl+C to stop")
     startStopCondition = StartStopCondition()
-    
-    #MAKING THREADS TO RUN READER AND WRITER OBJECTS
+
+    # MAKING THREADS TO RUN READER AND WRITER OBJECTS
     FuelReader = FuelGauge([Fuel, "Fuel", "FuelRemaining544645", FuelRL])  # noqa: F405
     DistWriter = MilesWriter([Miles, "Miles", "MilesTraveled"], DistTrav(0))  # noqa: F405
 
@@ -60,8 +61,8 @@ def main():
     # Add readers and start threads
     FuelThread = Thread(target=(FuelReader.run), daemon=True)
     DistThread = Thread(target=(DistWriter.run), args=(startStopCondition,), daemon=True)
-    
-    #REAL TIME READ FLAG DATA FROM FUEL IS HERE
+
+    # REAL TIME READ FLAG DATA FROM FUEL IS HERE
     CalcThread = Thread(target=(fuelConnectionStatus),
                         args=(
                             readers[0].dataQueue,
@@ -80,4 +81,3 @@ def main():
 
 
 main()
-
