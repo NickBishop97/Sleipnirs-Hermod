0015921c228c1079ec5795c4a9bbbcee769ece35
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-13 11:20:35 -0400

    Linted Writers.py

--- Vehicle-Python/ADTs/Writers.py
+++ Vehicle-Python/ADTs/Writers.py
@@ -1,18 +1,20 @@
 # from threading import Condition
 import time
-import random
+# import random
 
-#ADT IMPORTS
-from entity import Entity  
-from Calculators import *
+# ADT IMPORTS
+from entity import Entity
+from Calculators import *  # noqa F403 (linting exemption for import)
 
-global_sleep_time          = float(0.25)
+global_sleep_time = float(0.25)
 global_sleep_time_buffered = global_sleep_time + float(0.03)
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
+
 class FuelWriter(Entity.Writer):
     def __init__(self, ddsDataArray, CalculationClass):
 
@@ -20,7 +22,7 @@ class FuelWriter(Entity.Writer):
         super().__init__(ddsDataArray)
 
     def write(self, stop_flag):
-        
+
         # UPDATING MESSAGE CONTENTS
         dataOutput = self.CalculationClass.consumeFuel(stop_flag)
         self.data.litersRemaining(float(dataOutput[0]))
@@ -31,8 +33,8 @@ class FuelWriter(Entity.Writer):
         print(f"{self.index}, {dataOutput[0]}, {dataOutput[1]} \n")
         self.index = self.index + 1
 
-    def run(self, stop_flag) -> None: 
-        #self.wait_discovery()
+    def run(self, stop_flag) -> None:
+        # self.wait_discovery()
         while True:
             self.write(stop_flag)
             time.sleep(global_sleep_time)
@@ -42,6 +44,8 @@ class FuelWriter(Entity.Writer):
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
+
 class MilesWriter(Entity.Writer):
     def __init__(self, ddsDataArray, CalculationClass):
         self.CalculationClass = CalculationClass
@@ -53,12 +57,12 @@ class MilesWriter(Entity.Writer):
         self.data.milesTraveled(float(self.CalculationClass.milesTraveled))
         self.data.index(self.index)
         self.writer.write(self.data)
-        
+
         print(f"MILES TRAVELED: {self.data.index()}, {self.data.milesTraveled()}\n")
         self.index += 1
 
     def run(self, startStopCondition):
-        #self.wait_discovery()
+        # self.wait_discovery()
         while True:
             self.write(startStopCondition)
             time.sleep(global_sleep_time)    # Report every 0.25 s
@@ -71,88 +75,88 @@ class MilesWriter(Entity.Writer):
 ############################################################################################
 ############################################################################################
 
+
 class LowFuelWriter(Entity.Writer):
     def __init__(self, ddsDataArray):
         super().__init__(ddsDataArray)
 
     def write(self, fuelQueue):
-        lowFuel = LowFuelCalc(50)
+        lowFuel = LowFuelCalc(50)  # noqa F405 (linting exemption from Calculators import)
         alert = lowFuel.lowFuelAlert(fuelQueue.get()[1])
         # UPDATING MESSAGE CONTENTS
         self.data.isFuelLow(alert)
         self.data.index(self.index)
         self.writer.write(self.data)
         print(f"Low Fuel: {self.data.index()}, {bool(self.data.isFuelLow())}\n")
-            
+
         self.index = self.index + 1
 
     def run(self, fuelQueue):
-        #self.wait_discovery()
+        # self.wait_discovery()
         while True:
             self.write(fuelQueue)
             time.sleep(global_sleep_time)
-            
+
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
 
 class MpGWriter(Entity.Writer):
-    def __init__(self, ddsDataArray): #, CalculationClass):
+    def __init__(self, ddsDataArray):  # , CalculationClass):
 
-        #self.CalculationClass = CalculationClass
+        # self.CalculationClass = CalculationClass
         super().__init__(ddsDataArray)
 
     def write(self, fuelQueue, milesQueue):
-        mpgCalc = MpGCalc()
+        mpgCalc = MpGCalc()  # noqa F405 (linting exemption from Calculators import)
 
         self.data.index(self.index)
         self.data.mpg(mpgCalc.calculateMpG(fuelQueue, milesQueue))
-        
+
         self.writer.write(self.data)
-        
+
         print(f"MpG: {self.data.index()}, {self.data.mpg()} \n")
         self.index = self.index + 1
-            
-            
+
     def run(self, fuelQueue, milesQueue):
-        #self.wait_discovery()
+        # self.wait_discovery()
         while True:
             self.write(fuelQueue, milesQueue)
             time.sleep(global_sleep_time_buffered)
-  
+
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
 
 class MilesRemaining(Entity.Writer):
-    def __init__(self, ddsDataArray): #, CalculationClass):
+    def __init__(self, ddsDataArray):  # , CalculationClass):
 
-        #self.CalculationClass = CalculationClass
+        # self.CalculationClass = CalculationClass
         super().__init__(ddsDataArray)
 
     def write(self, fuelQueue, mpgQueue):
 
-        milesRemainCalc = MileRemainCalc()
-        
+        milesRemainCalc = MileRemainCalc()  # noqa F405 (linting exemption)
+
         self.data.index(self.index)
         self.data.milesToRefuel(milesRemainCalc.calculateMpG(fuelQueue, mpgQueue))
         self.writer.write(self.data)
-        
+
         if not fuelQueue.empty() or not mpgQueue.empty():
             print(f"MilesToRefuel: {self.data.index()}, {self.data.milesToRefuel()} \n")
         else:
             print("MilesToRefuel:")
-            
+
         self.index = self.index + 1
-            
-            
+
     def run(self, fuelQueue, mpgQueue):
-        #self.wait_discovery()
+        # self.wait_discovery()
         while True:
             self.write(fuelQueue, mpgQueue)
             time.sleep(global_sleep_time_buffered)
-
