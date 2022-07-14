92ca27dd0cbd60c73142e37aa7aca445486c8f71
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-14 17:18:39 -0400

    adding stuff to dashboard

--- Examples/Trip_Meter/Cole/Dashboard.py
+++ Examples/Trip_Meter/Cole/Dashboard.py
@@ -1,6 +1,16 @@
 import TripMeter
 
 
+class StickButton:
+    def __init__(self):
+        self.longPress = False
+        self.shortPress = True
+
 class Dashboard:
     def __init__(self):
         self.tm = TripMeter()
+        self.b = StickButton()
+        self.fg = FuelGauge()
+        self.m = MilesSensor()
+
+    def 
