dd46d1baa66088cefc147a1a395edb0f19d59688
n16743 <nicholas.bishop2@ngc.com>
2022-07-14 09:56:13 -0400

    Updated Examples folder

--- Examples/Trip_Meter/N16743/TripMeter.py
+++ Examples/Trip_Meter/N16743/TripMeter.py
@@ -9,17 +9,12 @@ class TripData:
         self.avgspeed = SP
         self.time = TIME
 
-    def setMPG(self, MPG):
-        self.mpg = MPG
-
-    def setMT(self, MT):
-        self.milesTraveled = MT
-
-    def setSP(self, SP):
-        self.avgspeed = SP
-
-    def setTIME(self, TIME):
-        self.time = TIME
+    def updateData(self, MT, MPG, SP, TIME):
+        if MT >= 0 and MPG >= 0 and SP >= 0 and TIME >= 0:
+            self.milesTraveled = MT
+            self.mpg = MPG
+            self.avgspeed = SP
+            self.time = TIME
 
     def resetData(self):
         self.milesTraveled = 0.0
@@ -32,17 +27,38 @@ class TripMeter:
 
     # Constructor
     def __init__(self):
-
-        # Instance Variables
         self.currently_selected = 0
         self.trip1 = TripData(0.0, 0.0, 0.0, 0.0)
         self.trip2 = TripData(0.0, 0.0, 0.0, 0.0)
 
-    def setCS(self, CS):
-        self.currently_selected = CS
+    def curentTrip(self):
+        if self.currently_selected == 0:
+            return self.trip1
+        else:
+            return self.trip2
+
+    def updateTrip(self, MT, MPG, SP, TIME):
+        self.trip1.updateData(MT, MPG, SP, TIME)
+        self.trip2.updateData(MT, MPG, SP, TIME)
 
     def resetTrip(self, trip):
         trip.resetData()
+
+
+class Button:
+
+    def __init__(self, TM):
+        self.button = 0
+        self.TripMeter = TM
+
+    def click(self):
+        if self.TripMeter.currently_selected == 0:
+            self.TripMeter.currently_selected = 1
+        else:
+            self.TripMeter.currently_selected = 0
+
+    def longclick(self):
+        self.TripMeter.resetTrip(self.TripMeter.curentTrip())
 
 
 class DashBoard:
@@ -50,14 +66,28 @@ class DashBoard:
     # Constructor
     def __init__(self):
         self.TM = TripMeter()
+        self.BT = Button(self.TM)
+
+    def showTrip(self):
+        temp = self.TM.curentTrip()
+        print(temp.mpg, "Mpg")
+        print(temp.milesTraveled, "Mls")
+        print(temp.avgspeed, "Mph")
+        print(temp.time, "Hrs")
+
+    def checkTime(self):
+        temp = self.TM.curentTrip()
+        if temp.time >= 2:
+            # print("Drive Time over 2Hrs")
+            return 1
+        else:
+            return 0
 
 
-def main():
-    d = DashBoard()
-    d.TM.trip1.setMPG(1)
-    print(d.TM.trip1.mpg)
-    d.TM.trip1.resetTrip()
+# def main():
+    # d = DashBoard()
+    # d.showTrip()
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+    # main()
