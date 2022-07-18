b4ecc2a5239aada240b74c3677b0c2134279f786
Spencer Williams <Spencer.Williams3@ngc.com>
2022-07-18 11:38:06 -0400

    Creating directory and import for gym

--- Examples/Gym_Simulation/gymTradeStudy.py
+++ Examples/Gym_Simulation/gymTradeStudy.py
@@ -1,3 +1,15 @@
 import gym
 
 env = gym.make('MountainCarContinuous-v0')
+
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(1000):
+    observation, reward, done, info = env.step(env.action_space.sample())
+
+    if done:
+        observation, info = env.reset(return_info=True)
+
+env.close()
