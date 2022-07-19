25a8165ee18092d6d388be7e25ee601b520e2230
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-19 17:17:31 -0400

    Adding coverage artifacts to pipeline

--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -7,8 +7,10 @@ echo "================================================"
 echo "Starting unit tests for Vehicle-Python project..."
 echo "================================================"
 echo ""
-cd Vehicle-Python/Pytests
-pytest
+cp Vehicle-Python/Pytests/* Vehicle-Python ADTs
+cd Vehicle-Python/ADTs
+mkdir coverage
+pytest --cov=./ > coverage/ADT_cov_report.txt
 cd -
 
 echo ""
