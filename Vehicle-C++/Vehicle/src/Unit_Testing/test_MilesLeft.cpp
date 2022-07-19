f4887a4d3f3eca246023b53a5366e2f4edaf4212
Spencer Williams <Spencer.Williams3@ngc.com>
2022-07-19 16:30:16 -0400

    Creating unit tests for Miles Left

--- Vehicle-C++/Vehicle/src/Unit_Testing/test_MilesLeft.cpp
+++ Vehicle-C++/Vehicle/src/Unit_Testing/test_MilesLeft.cpp
@@ -5,17 +5,20 @@
 #include <boost/test/unit_test.hpp>
 #include "../Calculations.h"
 
+#define INIT_VALUE 0.0
 #define NEG_VALUE -0.001
+#define ERROR_CODE -1.0
 #define POS_VALUE 13.2354
 #define DIFF_POS_VALUE 21.54
+#define LARGER_THAN_TANK 0.001 + TANK_CAP
 #define LARGE_FR 226754.6785576899
 #define LARGE_MPG 223654.6735786819
 
 using namespace std;
 
 /** 
- * @brief Boost test to verify that when MPG is negative, 
- * then Miles remaining returns 0 
+ * @brief Assert that when MPG is negative, 
+ * then get_MilesLeft returns ERROR_CODE 
  *
  * @param MPG Miles per Gallon
  * @param FR Fuel Remaining 
@@ -24,12 +27,12 @@ using namespace std;
 BOOST_AUTO_TEST_CASE(negativeMPG) 
 {
     ML ml;
-    BOOST_CHECK(ml.get_MilesLeft(-0.001, 22) == 0);
+    BOOST_CHECK(ml.get_MilesLeft(NEG_VALUE, POS_VALUE) == ERROR_CODE);
 }
 
 /** 
- * @brief Boost test to verify that when MPG is 0, 
- * then Miles remaining returns 0 
+ * @brief Assert that when MPG is 0 or init, 
+ * then get_MilesLeft returns ERROR_CODE 
  *
  * @param MPG Miles per Gallon
  * @param FR Fuel Remaining 
@@ -38,12 +41,12 @@ BOOST_AUTO_TEST_CASE(negativeMPG)
 BOOST_AUTO_TEST_CASE(zeroMPG) 
 {
     ML ml;
-    BOOST_CHECK(ml.get_MilesLeft(5.0, 0) == 0);
+    BOOST_CHECK(ml.get_MilesLeft(INIT_VALUE, POS_VALUE) == ERROR_CODE);
 }
 
 /** 
- * @brief Boost test to verify that when FR is 0, 
- * then Miles remaining returns 0 
+ * @brief Assert that when FR is 0 or init, 
+ * then get_MilesLeft returns  
  *
  * @param MPG Miles per Gallon
  * @param FR Fuel Remaining 
@@ -52,12 +55,12 @@ BOOST_AUTO_TEST_CASE(zeroMPG)
 BOOST_AUTO_TEST_CASE(zeroFR) 
 {
     ML ml;
-    BOOST_CHECK(ml.get_MilesLeft(5.0, 0) == 0);
+    BOOST_CHECK(ml.get_MilesLeft(POS_VALUE, INIT_VALUE) == ERROR_CODE);
 }
 
 /** 
- * @brief Boost test to verify that when MPG is a double, 
- *  
+ * @brief Assert that when FR is negative, 
+ * then get_MilesLeft returns ERROR_CODE
  *
  * @param MPG Miles per Gallon
  * @param FR Fuel Remaining 
@@ -65,17 +68,31 @@ BOOST_AUTO_TEST_CASE(zeroFR)
  */
 BOOST_AUTO_TEST_CASE(negtaiveFR) 
 {
-    
+    ML ml;
+    BOOST_CHECK(ml.get_MilesLeft(POS_VALUE, NEG_VALUE) == ERROR_CODE);
 }
 
 /** 
- * @brief Boost test to verify that when MPG is -1,  
- *
+ * @brief Assert that when FR is larger than tank,  
+ * then get_MilesLeft returns ERROR_CODE
  * @param MPG Miles per Gallon
  * @param FR Fuel Remaining 
  * @return bool
  */
-BOOST_AUTO_TEST_CASE(extremeCheck) 
+BOOST_AUTO_TEST_CASE(excessiveFR) 
 {
-    
+    ML ml;
+    BOOST_CHECK(ml.get_MilesLeft(POS_VALUE, LARGER_THAN_TANK) == ERROR_CODE);
 }
+
+/** 
+ * @brief Assert that when FR is larger than tank,  
+ * then get_MilesLeft returns ERROR_CODE
+ * @param MPG Miles per Gallon
+ * @param FR Fuel Remaining 
+ * @return bool
+ */
+BOOST_AUTO_TEST_CASE(overflow) 
+{
+    //ML ml;
+    //BOOST_CHECK(ml.get_MilesLeft(POS_VALUE, LARGER_THAN_TANK) == ERROR_CODE);
