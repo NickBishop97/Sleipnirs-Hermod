068aa5ab342b843a31018870a27092ba11e8c27f
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-19 17:42:04 -0400

    Only commenting Lemuel's test

--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -22,45 +22,44 @@ cd -
 # cd Examples/Trip_Meter/lemuel
 # pytest
 # cd -
-# 
-# echo ""
-# echo "==========================================="
-# echo "Starting unit tests for Spencer Example..."
-# echo "==========================================="
-# echo ""
-# cd Examples/Trip_Meter/Spencer
+
+echo ""
+echo "==========================================="
+echo "Starting unit tests for Spencer Example..."
+echo "==========================================="
+echo ""
+cd Examples/Trip_Meter/Spencer
+pytest
+cd -
+
+echo ""
+echo "========================================"
+echo "Starting unit tests for Nick Example..."
+echo "========================================"
+echo ""
+cd Examples/Trip_Meter/Nick
+pytest
+cd -
+
+echo ""
+echo "========================================"
+echo "Starting unit tests for Cole Example..."
+echo "========================================"
+echo ""
+cd Examples/Trip_Meter/Cole
+pytest
+cd -
+
+echo ""
+echo "==========================================="
+echo "Starting unit tests for Maxwell Example..."
+echo "==========================================="
+echo ""
+# cd Examples/Trip_Meter/N13853
 # pytest
 # cd -
-# 
-# echo ""
-# echo "========================================"
-# echo "Starting unit tests for Nick Example..."
-# echo "========================================"
-# echo ""
-# cd Examples/Trip_Meter/Nick
-# pytest
-# cd -
-# 
-# echo ""
-# echo "========================================"
-# echo "Starting unit tests for Cole Example..."
-# echo "========================================"
-# echo ""
-# cd Examples/Trip_Meter/Cole
-# pytest
-# cd -
-# 
-# echo ""
-# echo "==========================================="
-# echo "Starting unit tests for Maxwell Example..."
-# echo "==========================================="
-# echo ""
-# # cd Examples/Trip_Meter/N13853
-# # pytest
-# # cd -
-# 
-# # Test for pytest/robotframework example HelloWorld
-# #cd Examples/Python/HelloWorld
-# #robot *.robot
-# #cd -
-# 
+
+# Test for pytest/robotframework example HelloWorld
+#cd Examples/Python/HelloWorld
+#robot *.robot
+#cd -
