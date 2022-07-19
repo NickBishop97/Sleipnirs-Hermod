f5aab45c2a97f4f066de20ebe31d3bcd4eec65da
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-19 18:12:36 -0400

    Uncommenting all tests

--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -14,14 +14,14 @@ pytest ./
 pytest --cov=./ > coverage/ADT_cov_report.txt
 cd -
 
-# echo ""
-# echo "========================================="
-# echo "Starting unit tests for Lemuel Example..."
-# echo "========================================="
-# echo ""
-# cd Examples/Trip_Meter/lemuel
-# pytest
-# cd -
+echo ""
+echo "========================================="
+echo "Starting unit tests for Lemuel Example..."
+echo "========================================="
+echo ""
+cd Examples/Trip_Meter/lemuel
+pytest
+cd -
 
 echo ""
 echo "==========================================="
@@ -41,14 +41,14 @@ cd Examples/Trip_Meter/Nick
 pytest
 cd -
 
-# echo ""
-# echo "========================================"
-# echo "Starting unit tests for Cole Example..."
-# echo "========================================"
-# echo ""
-# cd Examples/Trip_Meter/Cole
-# pytest
-# cd -
+echo ""
+echo "========================================"
+echo "Starting unit tests for Cole Example..."
+echo "========================================"
+echo ""
+cd Examples/Trip_Meter/Cole
+pytest
+cd -
 
 echo ""
 echo "==========================================="
