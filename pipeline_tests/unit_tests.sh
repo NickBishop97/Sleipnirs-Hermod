ff924a32fe3866e59f9fa8ac0d6c649323b847ba
n08066 <Matthew.Hendrickson2@ngc.com>
2022-07-19 17:36:40 -0400

    Commenting out experimental tests

--- pipeline_tests/unit_tests.sh
+++ pipeline_tests/unit_tests.sh
@@ -14,52 +14,53 @@ pytest ./
 pytest --cov=./ > coverage/ADT_cov_report.txt
 cd -
 
-echo ""
-echo "========================================="
-echo "Starting unit tests for Lemuel Example..."
-echo "========================================="
-echo ""
-cd Examples/Trip_Meter/lemuel
-pytest
-cd -
-
-echo ""
-echo "==========================================="
-echo "Starting unit tests for Spencer Example..."
-echo "==========================================="
-echo ""
-cd Examples/Trip_Meter/Spencer
-pytest
-cd -
-
-echo ""
-echo "========================================"
-echo "Starting unit tests for Nick Example..."
-echo "========================================"
-echo ""
-cd Examples/Trip_Meter/Nick
-pytest
-cd -
-
-echo ""
-echo "========================================"
-echo "Starting unit tests for Cole Example..."
-echo "========================================"
-echo ""
-cd Examples/Trip_Meter/Cole
-pytest
-cd -
-
-echo ""
-echo "==========================================="
-echo "Starting unit tests for Maxwell Example..."
-echo "==========================================="
-echo ""
-# cd Examples/Trip_Meter/N13853
+# echo ""
+# echo "========================================="
+# echo "Starting unit tests for Lemuel Example..."
+# echo "========================================="
+# echo ""
+# cd Examples/Trip_Meter/lemuel
 # pytest
 # cd -
-
-# Test for pytest/robotframework example HelloWorld
-#cd Examples/Python/HelloWorld
-#robot *.robot
-#cd -
+# 
+# echo ""
+# echo "==========================================="
+# echo "Starting unit tests for Spencer Example..."
+# echo "==========================================="
+# echo ""
+# cd Examples/Trip_Meter/Spencer
+# pytest
+# cd -
+# 
+# echo ""
+# echo "========================================"
+# echo "Starting unit tests for Nick Example..."
+# echo "========================================"
+# echo ""
+# cd Examples/Trip_Meter/Nick
+# pytest
+# cd -
+# 
+# echo ""
+# echo "========================================"
+# echo "Starting unit tests for Cole Example..."
+# echo "========================================"
+# echo ""
+# cd Examples/Trip_Meter/Cole
+# pytest
+# cd -
+# 
+# echo ""
+# echo "==========================================="
+# echo "Starting unit tests for Maxwell Example..."
+# echo "==========================================="
+# echo ""
+# # cd Examples/Trip_Meter/N13853
+# # pytest
+# # cd -
+# 
+# # Test for pytest/robotframework example HelloWorld
+# #cd Examples/Python/HelloWorld
+# #robot *.robot
+# #cd -
+# 
